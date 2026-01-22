bb6f09484ad84d9771626435ba98f23d1e0dd385
meloress <melores00@gmail.com>
2026-01-22 06:29:08 +0500

    Update main.py

--- main.py
+++ main.py
@@ -38,8 +38,8 @@ async def main():
     # =================================================
     scheduler = AsyncIOScheduler(timezone="Asia/Tashkent")
     
-    scheduler.add_job(send_scheduled_lessons, 'cron', hour=6, minute=24, args=[bot])
-    scheduler.add_job(send_scheduled_lessons, 'cron', hour=6, minute=25, args=[bot])
+    scheduler.add_job(send_scheduled_lessons, 'cron', hour=6, minute=30, args=[bot])
+    scheduler.add_job(send_scheduled_lessons, 'cron', hour=6, minute=31, args=[bot])
     
     scheduler.start()
     print("⏰ Жадвал (10:00 ва 16:00) ишга тушди...")
